0a5e0d46bc5d56f0d226d2c810b742da877d68a9
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-07-29 17:45:59 +0330

    add README.md

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/<int:id>/delete',views.admin_article_delete,name="admin-article-delete"),
     path('admin/<int:id>/edit',views.admin_article_edit,name="admin-article-edit"),
     path('admin/create',views.admin_article_create,name="admin-article-create"),
-    path('admin/',views.admin_index,name="admin-page"),
+    path('admin',views.admin_index,name="admin-page"),
 ]
